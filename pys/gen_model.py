21e22b1cbc8d1777094d597babe8ee92ccf83e00
RustRookie-coder <48434132+RustRookie-coder@users.noreply.github.com>
2025-02-15 15:37:53 +0800

    delete: remove pys file

--- pys/gen_model.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-# ResNet50 图片分类
-
-from tensorflow.keras.applications.resnet50 import (ResNet50,
-                                                    decode_predictions,
-                                                    preprocess_input)
-model = ResNet50(weights='imagenet')
-model.save('/Users/jifeiyu/Documents/Rust-frame-learn/pys/resnet50')
